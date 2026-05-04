be89f92238d8d44dc352995d844ca7ca9fded28d
iTsLJ <lucasjlopesferreira@gmail.com>
2026-05-04 18:51:24 -0300

    Initialize coinly-api project with Gradle setup.

---
+++ coinly-api/src/main/java/com/coinly/api/CoinlyApiApplication.java
@@ -0,0 +1,13 @@
+package com.coinly.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinlyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinlyApiApplication.class, args);
+	}
+
+}
