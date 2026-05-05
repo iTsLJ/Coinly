1622107680ec5a77486ea9d46334bdf595631cc7
iTsLJ <lucasjlopesferreira@gmail.com>
2026-05-05 20:39:56 -0300

    Revert "front inicial"
    This reverts commit 5fc8913a34a6744c7e8cbe9d48ad21a045d7ff88.

--- coinly-web/src/services/instituicaoService.ts
+++
@@ -1,8 +0,0 @@
-import { http } from './http'
-import type { Instituicao } from '../types/api'
-
-const BASE = '/api/instituicoes'
-
-export const instituicaoService = {
-  listar: () => http.get<Instituicao[]>(BASE),
-}
